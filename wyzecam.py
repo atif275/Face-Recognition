90f8877197e0251639e060061d4d92f1bde3ff5b
atif275 <atifhanif275@gmail.com>
2024-06-07 19:35:18 +0500

    initial commit

---
+++ wyzecam.py
@@ -0,0 +1,14 @@
+import cv2
+
+video = cv2.VideoCapture("rtsp://Atif:27516515@192.168.1.9/live")
+
+
+while True:
+    _,frame = video.read()
+    cv2.imshow("RSTP",frame)
+    k = cv2.waitKey(1)
+    if k== ord('q'):
+        break
+
+video.release()
+cv2.distroyAllWindows()
